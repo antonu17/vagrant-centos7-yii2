0ae5d4d8f0c33e9a38ecbc1d51842db881af7d60
antonu17 <antonu17@gmail.com>
2016-02-02 12:12:26 +0600

    Update npm package

--- puppet/env/test/manifests/Nodejs.pp
+++ puppet/env/test/manifests/Nodejs.pp
@@ -1,5 +1,13 @@
 class my_nodejs {
-  class { 'nodejs': }
+
+  class { 'nodejs':
+  } ->
+
+  exec { 'sudo npm install npm -g':
+    path        => '/usr/bin',
+    refreshonly => true,
+    subscribe   => Package['nodejs'],
+  } ->
 
   package { 'forever':
     ensure   => 'present',
